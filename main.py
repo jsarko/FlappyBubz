44fbe184b0cada6c47adf1c14e2e85a05d39b039
Joe Sarko <joe.sarko@gmail.com>
2024-12-01 11:32:07 -0600

    commit

--- main.py
+++ main.py
@@ -20,12 +20,14 @@ class App:
     def on_init(self):
         pygame.init()
         pygame.font.init()
-        self.font = pygame.font.SysFont("impact", 55)
+        self.font_large = pygame.font.SysFont("impact", 55)
+        self.font_small = pygame.font.SysFont("impact", 35)
         self.screen = pygame.display.set_mode(self.size, vsync=1)
         self._running = True
         self.clock = pygame.time.Clock()
         self.player = Player(self.width / 2 - 200, self.height / 2)
         self.obstacles = []
+        self.collide_list = []
         pygame.time.set_timer(pygame.USEREVENT, 3000)
  
     def on_event(self, event):
@@ -39,30 +41,34 @@ class App:
         # All events that start once pregame ends
         elif not self.pregame:
             if event.type == pygame.USEREVENT:
-                self.obstacles.append(
-                    Obstacle(
-                        x=self.width / 2 + 300, 
-                        y=self.height,
-                        screen_height=self.height
-                    )
+                obs = Obstacle(
+                    x=self.width / 2 + 300, 
+                    y=self.height,
+                    screen_height=self.height
                 )
+                self.obstacles.append(obs)
+                self.collide_list += [obs.rect, obs.inverse_rect]
 
 
     def on_loop(self):
-        self.player.update(self.dt)
+        if not self.endgame:
+            self.player.update(self.dt)
 
-        # Update obstacles
-        for obstacle in self.obstacles:
-            obstacle.update(self.dt)
+            # Update obstacles
+            # print(f"Obstacles: {len(self.obstacles)}")
+            for obstacle in self.obstacles:
+                obstacle.update(self.dt)
+                # Check if obstacle is offscreen
+                if obstacle.isOffscreen():
+                    self.obstacles.pop(0)
+                # Check if player has passed obstacle and a point should be awarded
+                if (obstacle.rect.x + obstacle.rect.width) < self.player.rect.x and obstacle not in self.player.scored_obs:
+                    self.player.scored_obs.append(obstacle)
+                # print(f"ClosestObs: {obstacle.rect.x} / Player: {self.player.rect.x}")
 
-        # Check if obstacle is offscreen
-        if len(self.obstacles) > 0:
-            if self.obstacles[-1].isOffscreen():
-                self.obstacles.pop()
-        
-        # Check for collisions
-        if self.player.rect.collidelist(self.obstacles) != -1:
-            self.endgame = True
+            # Check for collisions
+            if self.player.rect.collidelist(self.collide_list) != -1:
+                self.endgame = True
             
 
     def on_render(self):
@@ -73,6 +79,10 @@ class App:
             self.player.draw(self.screen)
             for obstacle in self.obstacles:
                 obstacle.draw(self.screen)
+            
+            # Display Score
+            score = self.font_large.render(f"{self.player.get_score}", True, pygame.Color("black"))
+            self.screen.blit(score, (15, 15))
 
         pygame.display.flip()
 
@@ -80,16 +90,21 @@ class App:
         pygame.quit()
 
     def on_endgame(self):
-        print("ENDGANE")
-        
         self.screen.fill("white")
-        msg = self.font.render('BOOM BITCH!', True, pygame.Color("black"))
-        print(f"Height: {self.height}\nWidth: {self.width}")
+        msg = self.font_large.render(f'BOOM BITCH!', True, pygame.Color("black"))
+        score = self.font_small.render(f"SCORE: {self.player.get_score}", True, pygame.Color("black"))
         self.screen.blit(
             msg, 
             (
                 (self.width / 2) - (msg.get_width() / 2), 
                 (self.height / 2) - (msg.get_height() / 2)
+            )
+        )
+        self.screen.blit(
+            score,
+            (
+                (self.width / 2) - (score.get_width() / 2), 
+                (self.height / 2) - (score.get_height() / 2) + 50
             )
         )
  
@@ -107,7 +122,7 @@ class App:
             self.dt = 1 / self.clock.tick(120)
             # For the moment, removing the print causes
             # speed spikes intermitently.
-            print(f"dt: {self.dt}")
+            # print(f"dt: {self.dt}")
         self.on_cleanup()
  
 if __name__ == "__main__" :
--- objects.py
+++ objects.py
@@ -30,6 +30,14 @@ class Player(Object):
         super().__init__(x, y, 40, 40)
         self.color = (0, 0, 255)
         self.velocity = pygame.Vector2(0,0)
+        # scored_obs is a list of obstacles the player has passed,
+        # score is calculated from the number of objects in the list.
+        self.scored_obs = []
+        self.player_image = self.load_player_image()
+    
+    def draw(self, screen):
+        # pygame.draw.rect(self.player_image, self.color, self.rect)
+        screen.blit(self.player_image, (self.rect.x, self.rect.y))
 
     def jump(self):
         self.velocity.y = -5
@@ -37,6 +45,16 @@ class Player(Object):
     def update(self, dt):
         self.velocity.y += 3 * dt
         self.rect.move_ip(self.velocity)
+    
+    @staticmethod
+    def load_player_image():
+        image = pygame.image.load("assets/player.gif").convert_alpha()
+        image = pygame.transform.scale_by(image, 0.1)
+        return image
+    
+    @property
+    def get_score(self):
+        return len(self.scored_obs)
         
 
 class Obstacle(Object):
@@ -46,16 +64,17 @@ class Obstacle(Object):
         self.velocity = pygame.Vector2(0,0)
         self.rect.y = self.rect.y - self.rect.height
         self.obstacle2_height = 2
+        self.inverse_rect = pygame.Rect(self.rect.x, 0, self.rect.width, self.h2)
 
     def update(self, dt):
         self.velocity.x = -50 * dt
         self.rect.move_ip(self.velocity)
+        self.inverse_rect.move_ip(self.velocity)
 
     def draw(self, screen):
         pygame.draw.rect(screen, self.color, self.rect)
-        pygame.draw.rect(screen, self.color, 
-                         (self.rect.x, 0, self.rect.width, self.h2)
-                         )
+        pygame.draw.rect(screen, self.color, self.inverse_rect)
+
     def get_obstacle2_height(self, screen_height):
         # Calculates the height for the second generated obstacle
         max_combined_height = screen_height - OBSTACLE_GAP
